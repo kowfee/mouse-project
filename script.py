d3e887d94aaeb641a8374c9a726ec2aab9bf237a
Ali <aliilkergueler@googlemail.com>
2024-10-31 15:49:29 +0100

    fix bugs + add check if board is connected

--- script.py
+++ script.py
@@ -1,16 +1,14 @@
 import subprocess
 import threading
-from tkinter import * 
-from tkinter import ttk 
+from tkinter import *
+from tkinter import ttk
 from tkinter import colorchooser
 from sys import platform
 import serial.tools.list_ports
 
 def getValues():
-
-    fr = open("./mouse-project.ino", "r")
-    inoLines= fr.readlines()
-    fr.close
+    with open("./mouse-project.ino", "r") as ino_read:
+        inoLines = ino_read.readlines()
 
     nDPI = numberOfDPIs.get()
     s1 = ent1.get()
@@ -18,7 +16,7 @@ def getValues():
     s3 = ent3.get()
     s4 = ent4.get()
     s5 = ent5.get()
-    
+
     if s1 != '' :
         i1 = int(s1)
         nDPI1 = int((i1/100)-1)
@@ -31,69 +29,70 @@ def getValues():
     if s4 != '' :
         i4 = int(s4)
         nDPI4 = int((i4/100)-1)
-    if s5 != '' : 
+    if s5 != '' :
         i5 = int(s5)
         nDPI5 = int((i5/100)-1)
 
-    fw = open("./mouse-project.ino", "w")
+    with open("./mouse-project.ino", "w") as ino_write:
+        rgb_mode = modeRGBbox.get()
+        rgb_color = colorRGB
+        brightness = brightnessValue.get()
 
-    rgb_mode = modeRGBbox.get()
-    rgb_color = colorRGB
-    brightness = brightnessValue.get()
-    
-    red: int
-    green: int
-    blue: int
+        red: int
+        green: int
+        blue: int
 
-    if rgb_mode != 'Static':
-        red = 0
-        green = 0
-        blue = 0
-    else:
-        red = rgb_color[0]
-        green = rgb_color[1]
-        blue = rgb_color[2]
+        if rgb_mode != 'Static':
+            red = 0
+            green = 0
+            blue = 0
+        else:
+            red = rgb_color[0]
+            green = rgb_color[1]
+            blue = rgb_color[2]
 
-    inoLines[184] = f"#define GLOBAL_BRIGHTNESS {brightness}\n"
+        inoLines[184] = f"#define GLOBAL_BRIGHTNESS {brightness}\n"
 
-    if nDPI == 1:
-        inoLines[180] = f"int dpi_values[] = {{{nDPI1}}};\n"
-    elif nDPI == 2:
-        inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}}};\n"
-    elif nDPI == 3:
-        inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}}};\n"
-    elif nDPI == 4:
-        inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}, {nDPI4}}};\n"
-        inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}, {nDPI4}, {nDPI5}}};\n"
+        if nDPI == 1:
+            inoLines[180] = f"int dpi_values[] = {{{nDPI1}}};\n"
+        elif nDPI == 2:
+            inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}}};\n"
+        elif nDPI == 3:
+            inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}}};\n"
+        elif nDPI == 4:
+            inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}, {nDPI4}}};\n"
+        elif nDPI == 5:
+            inoLines[180] = f"int dpi_values[] = {{{nDPI1}, {nDPI2}, {nDPI3}, {nDPI4}, {nDPI5}}};\n"
 
-    if rgb_mode == "Off":
-        inoLines[188] = f"int rgb_selector = 0;\n"
-    elif rgb_mode == "Static":
-        inoLines[188] = f"int rgb_selector = 1;\n"
-        inoLines[364] = f"\tstrip.setPixelColor(i, {red}, {green}, {blue});\n"
-    elif rgb_mode == "Rainbow":
-        inoLines[188] = f"int rgb_selector = 2;\n"
-    elif rgb_mode == "Police":
-        inoLines[188] = f"int rgb_selector = 3;\n"
-    elif rgb_mode == "Breathing":
-        inoLines[188] = f"int rgb_selector = 4;\n"
+        if rgb_mode == "Off":
+            inoLines[188] = f"int rgb_selector = 0;\n"
+        elif rgb_mode == "Static":
+            inoLines[188] = f"int rgb_selector = 1;\n"
+            inoLines[364] = f"\tstrip.setPixelColor(i, {red}, {green}, {blue});\n"
+        elif rgb_mode == "Rainbow":
+            inoLines[188] = f"int rgb_selector = 2;\n"
+        elif rgb_mode == "Police":
+            inoLines[188] = f"int rgb_selector = 3;\n"
+        elif rgb_mode == "Breathing":
+            inoLines[188] = f"int rgb_selector = 4;\n"
 
-    fw.writelines(inoLines)
-    fw.close()
+        ino_write.writelines(inoLines)
 
     uploadToBoard()
- 
+
 def uploadToBoard():
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
         if "2E8A:00C0 SER=048C63E43659285F" in p.hwid:
             arduino_port = p.device
+    if checkBoardConnection():
+        if platform == "win32" or platform == "win64":
+            subprocess.run(["arduino-cli.exe", "compile", "--fqbn", "arduino:mbed_rp2040:pico",  "--port", arduino_port, "--upload", "./mouse-project.ino"])
+        elif platform == "linux" or platform == "linux2":
+            subprocess.run(["./bin/arduino-cli", "compile", "--fqbn", "arduino:mbed_rp2040:pico",  "--port", arduino_port, "--upload", "./mouse-project.ino"])
+    else:
+        ErrorWindow()
 
-    if platform == "win32" or platform == "win64":
-        subprocess.run(["arduino-cli.exe", "compile", "--fqbn", "arduino:mbed_rp2040:pico",  "--port", arduino_port, "--upload", "./mouse-project.ino"])
-    elif platform == "linux" or platform == "linux2":
-        subprocess.run(["./bin/arduino-cli", "compile", "--fqbn", "arduino:mbed_rp2040:pico",  "--port", arduino_port, "--upload", "./mouse-project.ino"])
-    
 def checkValues(*args):
     dpi1 = ent1.get()
     dpi2 = ent2.get()
@@ -104,48 +103,42 @@ def checkValues(*args):
     color = colorRGB
     mode = modeRGBbox.get()
     scale = brightnessValue.get()
-    
+
     saveSettings.config(state=DISABLED)
 
     rgbValidation = (mode != "Static" and color == ()) or (mode == "Static" and color != ()) or (mode != "Static" and color != ())
-    dpiValidation = ((nDPI == 5 and dpi1 != "" and dpi2 != "" and dpi3 != "" and dpi4 != "" and dpi5 != "") 
-                        or (nDPI == 4 and dpi1 != "" and dpi2 != "" and dpi3 != "" and dpi4 != "") 
+    dpiValidation = ((nDPI == 5 and dpi1 != "" and dpi2 != "" and dpi3 != "" and dpi4 != "" and dpi5 != "")
+                        or (nDPI == 4 and dpi1 != "" and dpi2 != "" and dpi3 != "" and dpi4 != "")
                             or (nDPI == 3 and dpi1 != "" and dpi2 != "" and dpi3 != "")
-                                or (nDPI == 2 and dpi1 != "" and dpi2 != "") 
+                                or (nDPI == 2 and dpi1 != "" and dpi2 != "")
                                     or (nDPI == 1 and dpi1 != "") )
 
     if dpiValidation and rgbValidation:
         saveSettings.config(state="normal")
-    else: 
+    else:
         saveSettings.config(state=DISABLED)
 
-    if mode == 'Off':
+    if mode == 'Off' or mode == 'Breathing':
         brightnessValue.config(from_=0)
         brightnessValue.set(0)
         brightnessValue.config(state=DISABLED)
-    elif mode == 'Breathing':
-        brightnessValue.config(from_=0)
-        brightnessValue.set(0)
-        brightnessValue.config(state=DISABLED)
-    else: 
+    else:
         brightnessValue.config(state="normal", from_=10)
         if scale != 0:
             brightnessValue.set(scale)
         else:
             brightnessValue.set(100)
 
-    if mode != "Static":
-        colorFrame.config(bg=lab1['bg'])
-    else: 
+    if mode == "Static" and colorRGB != ():
         colorFrame.config(bg=('#%02x%02x%02x' % (colorRGB)))
-
-    #TODO: add values for the different modes (rainbow, police, breathing)
+    else:
+        colorFrame.config(bg=lab1['bg'])
 
 def addDPI(*args):
     n = numberOfDPIs.get()
 
     if n == 1:
-        ent1.config(state="readonly", style= "whiteBG.TCombobox")   
+        ent1.config(state="readonly", style= "whiteBG.TCombobox")
         ent2.set("")
         ent2.config(state="disabled", style="TCombobox")
         ent3.set("")
@@ -154,7 +147,7 @@ def addDPI(*args):
         ent4.config(state="disabled", style="TCombobox")
         ent5.set("")
         ent5.config(state="disabled", style="TCombobox")
-    elif n == 2: 
+    elif n == 2:
         ent1.config(state="readonly", style= "whiteBG.TCombobox")
         ent2.config(state="readonly", style= "whiteBG.TCombobox")
         ent3.set("")
@@ -178,7 +171,7 @@ def addDPI(*args):
         ent4.config(state="readonly", style= "whiteBG.TCombobox")
         ent5.set("")
         ent5.config(state="disabled", style="TCombobox")
-    elif n == 5: 
+    elif n == 5:
         ent1.config(state="readonly", style= "whiteBG.TCombobox")
         ent2.config(state="readonly", style= "whiteBG.TCombobox")
         ent3.config(state="readonly", style= "whiteBG.TCombobox")
@@ -197,7 +190,7 @@ def addDPI(*args):
         ent5.config(state="disabled", style="TCombobox")
 
 def selectColor():
-    global colorRGB 
+    global colorRGB
     color=colorchooser.askcolor(title="Farbe auswählen")
     colorHex = color[1]
     colorRGB = color[0]
@@ -214,6 +207,17 @@ def checkModeRGB(*args):
 
     checkValues()
 
+def checkBoardConnection():
+    if platform == "win32" or platform == "win64":
+        board_list = str(subprocess.run(["arduino-cli.exe","board", "list"], capture_output=True))
+    elif platform == "linux" or platform == "linux2":
+        board_list = str(subprocess.run(["bin/arduino-cli","board", "list"], capture_output=True))
+
+    if "arduino:mbed_rp2040:pico" in board_list:
+        return True
+    else:
+        return False
+
 root = Tk()
 style = ttk.Style()
 
@@ -221,10 +225,31 @@ colorRGB = ()
 
 style.map("whiteBG.TCombobox", fieldbackground=[("readonly", "white")])
 
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
+def ErrorWindow():
+    errorWindow = Toplevel(root)
+    errorWindow.title("Error")
+    errorWindow.geometry("300x90")
+    errorWindow.minsize(300, 90)
+    errorWindow.maxsize(300, 90)
+    Label(errorWindow, text="\nThere seems to be no device connected.\nPlease connect the device and try again!").pack()
+    Button(errorWindow, text="OK", command=errorWindow.destroy).pack()
+    center_window(errorWindow)
+
 root.title("Settings")
 root.minsize(500, 300)
 root.maxsize(500, 300)
-root.geometry("50x300+700+100")
+root.geometry("500x300")
+center_window(root)
 
 #dropdown menu option
 DPIamountOptions = [
@@ -379,7 +404,7 @@ numberOfDPIs = IntVar()
 numberOfDPIs.set(1)
 
 #dropdown label + menu
-dropDownLabel = Label(root, text="Amount DPI:").grid(row = 0, column = 0, pady=5, padx=5 )
+dropDownLabel = Label(root, text="# of DPIs:").grid(row = 0, column = 0, pady=5, padx=5 )
 dropDown = OptionMenu(root, numberOfDPIs, *DPIamountOptions, command=addDPI).grid(row = 0, column = 1, pady=5, padx=5 )
 
 #comboboxes with labels
